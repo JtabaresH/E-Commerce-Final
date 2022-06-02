7dddd9048785752188b1f3785e1d0ceb6c7a3e22
JtabaresH <jtabaresh@gmail.com>
2022-06-01 19:28:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bku2sg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bku2sg)
---
+++ src/store/slices/first.slice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// Cambiamos mySlice por el nombre de nuestro slice (usersSlice, toDosSlice...)
+export const mySlice = createSlice({
+  name: '',
+  initialState: undefined,
+  reducers: {},
+});
+
+export default mySlice.reducer;
