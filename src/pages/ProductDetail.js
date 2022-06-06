32ddd7644bd6b97ca89424d278c41047c6e9b621
JtabaresH <jtabaresh@gmail.com>
2022-06-05 19:04:45 -0500

    1 file modified

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { filterCategory } from '../store/slices/products.slice';
 
 const ProductDetail = () => {
-  return (
-    <h1>ProductDetail</h1>
-  )
-}
+  const [product, setProduct] = useState({});
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+
+  useEffect(() => {
+    axios
+      .get(`https://ecommerce-api-react.herokuapp.com/api/v1/products/${id}`)
+      .then((res) => {
+        const productSearched = res.data.find(
+          (productItem) => productItem.id === Number(id)
+        );
+        setProduct(productSearched);
+        dispatch(filterCategory(res.data.data?.product));
+      });
+  }, [dispatch, id]);
+
+  return <h1>ProductDetail</h1>;
+};
 
 export default ProductDetail;
