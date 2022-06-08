768649a79c4638403008486bfe3a0af85b66e6d0
JtabaresH <jtabaresh@gmail.com>
2022-06-07 22:00:22 -0500

    1 file modified

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,25 +1,27 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { filterTypeProduct } from '../store/slices/products.slice';
+import { filterCategory } from '../store/slices/products.slice';
 
 const ProductDetail = () => {
   const [product, setProduct] = useState({});
   const { id } = useParams();
   const dispatch = useDispatch();
-  const productList = useSelector((state) => state.product);
+  const navigate = useNavigate();
+
+  const productsList = useSelector((state) => state.products);
 
   useEffect(() => {
     axios
-      .get(`https://ecommerce-api-react.herokuapp.com/api/v1/products/`)
+      .get('https://ecommerce-api-react.herokuapp.com/api/v1/products')
       .then((res) => {
-        const productSearched = res.data.find(
-          (productItem) => productItem.id === Number(id)
+        const productSearched = res.data.data.products.find(
+          (productsItem) => productsItem.id === Number(id)
         );
         console.log(productSearched);
-        /* setProduct(productSearched);
-        dispatch(filterCategory(res.data)); */
+        setProducts(productSearched);
+        dispatch(filterCategory(productSearched.category.id));
       });
   }, [dispatch, id]);
 
@@ -28,7 +30,7 @@ const ProductDetail = () => {
   return (
     <div className="card">
       <h1>ProductDetail</h1>
-      <h1>{product.data?.product.title}</h1>
+      <h1>{product.title}</h1>
     </div>
   );
 };
