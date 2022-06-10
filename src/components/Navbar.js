38bd9c9d3ca508266c1932e6eb6c65c567b2e760
JtabaresH <jtabaresh@gmail.com>
2022-06-10 07:25:08 -0500

    4 files modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,6 @@ import CartSidebar from './CartSidebar';
 const Navbar = () => {
   const navigate = useNavigate();
 
-  const logout = () => {
-    localStorage.setItem('token', '');
-    alert('Successfully closed session');
-    navigate('/');
-  };
-
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -29,49 +23,26 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/#/">
-            Tech-Commerce
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-list"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-              />
-            </svg>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
+          <div className="row">
+            <div className="col border">
+              <a className="nav-link" href="/#/">
+                <i className="bi bi-cpu">
+                  <b> TechShop</b>
+                </i>
+              </a>
+            </div>
+            <div className="col border">
+              <div className="d-flex gap-3">
                 <a
                   className="nav-link active"
                   aria-current="page"
                   href="/#/login"
                 >
-                  Login
+                  <i className="bi bi-person-badge">{/* Login */}</i>
                 </a>
-              </li>
-              <li className="nav-item">
                 <a className="nav-link" href="/#/purchases">
-                  Purchases
+                  <i className="bi bi-bag">{/* Purchases */}</i>
                 </a>
-              </li>
-              <li className="nav-item">
                 <a
                   className="nav-link"
                   data-bs-toggle="offcanvas"
@@ -80,15 +51,10 @@ const Navbar = () => {
                   style={{ cursor: 'pointer' }}
                   onClick={handleShow}
                 >
-                  Cart
+                  <i class="bi bi-cart">{/* Shopping Cart */}</i>
                 </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" role="button" onClick={logout}>
-                  Log Out
-                </a>
-              </li>
-            </ul>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,8 +6,12 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
-
   const navigate = useNavigate();
+  const logout = () => {
+    localStorage.setItem('token', '');
+    alert('Successfully closed session');
+    navigate('/');
+  };
 
   const submit = (data) => {
     axios
@@ -33,40 +37,47 @@ const Login = () => {
       <Card style={{ maxWidth: '500px' }} className="mx-auto">
         <Card.Body>
           <h1 className="text-center">Login</h1>
-          <div className="card">
-            <h6>User Test</h6>
-            <p>
+          <div className="card mb-2">
+            <h6 className="text-center mt-1">User Test</h6>
+            <p className="m-3">
               <b>Email: </b>mason@gmail.com
             </p>
-            <p>
+            <p className="m-3 mt-0">
               <b>Password: </b>mason1234
             </p>
           </div>
-          <Form onSubmit={handleSubmit(submit)}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                {...register('email')}
-                type="email"
-                placeholder="Enter email"
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+          <div className="card">
+            <Form onSubmit={handleSubmit(submit)}>
+              <Form.Group className="m-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  {...register('email')}
+                  type="email"
+                  placeholder="Enter email"
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                {...register('password')}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+              <Form.Group className="m-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  {...register('password')}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Group>
+              <div className="d-flex justify-content-center mb-3 gap-2">
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+                <button className="btn btn-danger" onClick={logout}>
+                  Log Out
+                </button>
+              </div>
+            </Form>
+          </div>
         </Card.Body>
       </Card>
     </div>
