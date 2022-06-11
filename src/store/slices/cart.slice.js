b406745bb4e1be9e9fbd8cae7fbd860c7a65ca6d
JtabaresH <jtabaresh@gmail.com>
2022-06-10 22:18:49 -0500

    1 file modified

--- src/store/slices/cart.slice.js
+++ src/store/slices/cart.slice.js
@@ -1,37 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { setIsLoading } from './isLoading.slice';
 import getConfig from '../../utils/getConfig';
+import { setIsLoading } from './isLoading.slice';
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState: [],
   reducers: {
-    setProducts: (state, action) => {
+    setCart: (state, action) => {
       return action.payload;
     },
   },
 });
 
-export const { setProducts } = cartSlice.actions;
+export const { setCart } = cartSlice.actions;
 
-export const getProducts = () => (dispatch) => {
+export const getCart = () => (dispatch) => {
   dispatch(setIsLoading(true));
   return axios
     .get('https://ecommerce-api-react.herokuapp.com/api/v1/cart', getConfig())
-    .then((res) => dispatch(setProducts(res.data)))
-    .finally(() => dispatch(setIsLoading(false)));
-};
-
-export const addToCart = (product) => (dispatch) => {
-  dispatch(setIsLoading(true));
-  return axios
-    .post(
-      'https://ecommerce-api-react.herokuapp.com/api/v1/cart',
-      product,
-      getConfig()
-    )
-    .then(() => dispatch(getProducts()))
+    .then((res) => dispatch(setCart(res.data.data.cart.products)))
     .finally(() => dispatch(setIsLoading(false)));
 };
 
